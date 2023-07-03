6aaef4ef632d7520041bed03de1e93050518eb1d
mrsicsasa <sasamrsic20@gmail.com>
2023-07-03 14:32:09 +0200

    GetURLsFromHTML and tests

--- crawl.js
+++ crawl.js
@@ -1,3 +1,34 @@
+const{JSDOM}=require('jsdom')
+const { html } = require('parse5')
+function getURLsFromHTML(htmlBody,baseURL){
+    const urls=[]
+    const dom=new JSDOM(htmlBody)
+    const linkElements=dom.window.document.querySelectorAll('a')
+    for(linkElement of linkElements){
+        if(linkElement.href.slice(0,1)==="/"){
+           try{
+            const urlObj=new URL(`${baseURL}${linkElement.href}`)
+            urls.push(urlObj.href)
+           }
+           catch(err){
+            console.log(err.message)
+           }
+        }
+        else{
+            try{
+                const urlObj=new URL(linkElement.href)
+                urls.push(urlObj.href)
+               }
+               catch(err){
+                console.log(err.message)
+               }
+            
+        }
+    }
+    return urls
+}
+
+
 function normalizeURL(urlString){
     const urlObj=new URL(urlString)
     const hostPath= `${urlObj.hostname}${urlObj.pathname}`
@@ -9,5 +40,6 @@ function normalizeURL(urlString){
     }
 }
 module.exports={
-    normalizeURL
+    normalizeURL,
+    getURLsFromHTML
 }
